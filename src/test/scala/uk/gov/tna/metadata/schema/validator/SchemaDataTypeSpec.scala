0cd3b3dac1878f12fc12e09016b1a97f0c7d6adf
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2024-05-07 14:45:37 +0100

    more review changes

--- src/test/scala/uk/gov/tna/metadata/schema/validator/SchemaDataTypeSpec.scala
+++ src/test/scala/uk/gov/tna/metadata/schema/validator/SchemaDataTypeSpec.scala
@@ -8,6 +8,7 @@ import org.scalatest.wordspec.AnyWordSpec
 import java.io.{File, InputStream}
 import java.nio.file.Files
 import java.util
+import scala.jdk.CollectionConverters._
 
 class SchemaDataTypeSpec extends AnyWordSpec {
 
@@ -27,15 +28,17 @@ class SchemaDataTypeSpec extends AnyWordSpec {
 
     "fail when document is closed and missing closure property when using closureSchema" in {
       val schemaPath = "metadata-schema/closureSchema.schema.json"
-      val dataPath = "/data/testData.json"
+      val dataPath = "/data/testDataClosed.json"
       val schemaInputStream = Files.newInputStream(new File(schemaPath).toPath)
       val schema = getJsonSchemaFromStreamContentV7(schemaInputStream)
       val dataInputStream = getClass.getResourceAsStream(dataPath)
       val node = getJsonNodeFromStreamContent(dataInputStream)
 
       val errors: util.Set[ValidationMessage] = schema.validate(node.toPrettyString, InputFormat.JSON)
-      
-      errors.iterator().next().getMessage shouldBe "$: required property 'description_closed' not found"
+      val errorsArray = errors.asScala.toArray
+
+      errorsArray(0).getMessage shouldBe "$.foi_exemption_code[2]: does not have a value in the enumeration [23, 24, 26, 27(1), 27(2), 28, 29, 30(1), 30(2), 31, 32, 33, 34, 35(1)(a), 35(1)(b), 35(1)(c), 35(1)(d), 36, 37(1)(a), 37(1)(aa), 37(1)(ac), 37(1)(ad), 37(1)(b), 38, 39, 40(2), 41, 42, 43, 43(1), 43(2), 44, EIRs 12(3) & 13, EIR 12(5)(a), EIR 12(5)(b), EIR 12(5)(c), EIR 12(5)(d), EIR 12(5)(e), EIR 12(5)(f), EIR 12(5)(g)]"
+      errorsArray(1).getMessage shouldBe "$: required property 'description_closed' not found"
     }
 
     "pass an open document when using closureSchema" in {
