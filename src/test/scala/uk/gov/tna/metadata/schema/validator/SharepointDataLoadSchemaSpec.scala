c2e077e4c99d102b901427b724f32ea85aa1c7e2
TomJKing <tom.king@nationalarchives.gov.uk>
2024-06-28 13:18:28 +0100

    Fix schema referencing

--- src/test/scala/uk/gov/tna/metadata/schema/validator/SharepointDataLoadSchemaSpec.scala
+++ src/test/scala/uk/gov/tna/metadata/schema/validator/SharepointDataLoadSchemaSpec.scala
@@ -39,5 +39,35 @@ class SharepointDataLoadSchemaSpec extends BaseSpec {
       val errorsArray = errors.asScala.toArray
       errorsArray(0).getMessage shouldBe "$: required property 'date_last_modified' not found"
     }
+
+    "fail when 'file_size' is less than 0" in {
+      val schemaPath = "metadata-schema/dataLoadSharePointSchema.schema.json"
+      val dataPath = "/data/sharepointDataLoadInvalidFileSize.json"
+      val schemaInputStream = Files.newInputStream(new File(schemaPath).toPath)
+      val schema = getJsonSchemaFromStreamContentV7(schemaInputStream)
+      val dataInputStream = getClass.getResourceAsStream(dataPath)
+      val node = getJsonNodeFromStreamContent(dataInputStream)
+
+      val errors: util.Set[ValidationMessage] = schema.validate(node.toPrettyString, InputFormat.JSON)
+
+      errors.size() shouldBe 1
+      val errorsArray = errors.asScala.toArray
+      errorsArray(0).getMessage shouldBe "$.file_size: must have a minimum value of 0"
+    }
+
+    "fail when 'file_path' is less than 1" in {
+      val schemaPath = "metadata-schema/dataLoadSharePointSchema.schema.json"
+      val dataPath = "/data/sharepointDataLoadInvalidFilePath.json"
+      val schemaInputStream = Files.newInputStream(new File(schemaPath).toPath)
+      val schema = getJsonSchemaFromStreamContentV7(schemaInputStream)
+      val dataInputStream = getClass.getResourceAsStream(dataPath)
+      val node = getJsonNodeFromStreamContent(dataInputStream)
+
+      val errors: util.Set[ValidationMessage] = schema.validate(node.toPrettyString, InputFormat.JSON)
+
+      errors.size() shouldBe 1
+      val errorsArray = errors.asScala.toArray
+      errorsArray(0).getMessage shouldBe "$.file_path: must be at least 1 characters long"
+    }
   }
 }
