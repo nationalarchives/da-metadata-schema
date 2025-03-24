0ff665cc025fc69dc3b4fb2380a45025c5385c5c
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2025-03-24 15:10:58 +0000

    Tdrd 646 validate description for alternative description (#62)
    * Relationship schema checking description set if alternate description set

--- src/test/scala/uk/gov/tna/metadata/schema/validator/RelationshipSchemaSpec.scala
+++ src/test/scala/uk/gov/tna/metadata/schema/validator/RelationshipSchemaSpec.scala
@@ -5,6 +5,7 @@ import org.scalatest.matchers.should.Matchers.*
 import org.scalatest.wordspec.AnyWordSpec
 
 import java.util
+import scala.jdk.CollectionConverters.*
 
 class RelationshipSchemaSpec extends BaseSpec {
   "relationship schema validation" should {
@@ -14,8 +15,9 @@ class RelationshipSchemaSpec extends BaseSpec {
       val schemaSetup = createSchema(schemaPath, testDataPath)
 
       val errors: util.Set[ValidationMessage] = schemaSetup._1.validate(schemaSetup._2.toPrettyString, InputFormat.JSON)
-      errors.size() shouldBe 1
-      errors.iterator().next().getMessage shouldBe "$: required property 'file_name_translation_language' not found"
+      assert(errors.size == 1)
+      val errorsList = errors.asScala.toList
+      assert(errorsList.head.getMessage == "$: required property 'description' not found")
     }
   }
 }
