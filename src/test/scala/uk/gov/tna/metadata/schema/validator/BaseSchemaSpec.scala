cf18eee1bddc4bfa2ccbaa268108bd87ba53339a
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2025-05-13 11:45:42 +0100

    tidy up

--- src/test/scala/uk/gov/tna/metadata/schema/validator/BaseSchemaSpec.scala
+++ src/test/scala/uk/gov/tna/metadata/schema/validator/BaseSchemaSpec.scala
@@ -1,41 +1,53 @@
 package uk.gov.tna.metadata.schema.validator
 
-import com.networknt.schema.*
+import com.networknt.schema.{InputFormat, ValidationMessage}
 import org.scalatest.matchers.should.Matchers.*
 import org.scalatest.wordspec.AnyWordSpec
 
+import java.io.InputStream
 import java.util
+import scala.io.Source
 import scala.jdk.CollectionConverters.*
+import scala.util.Using
 
 class BaseSchemaSpec extends BaseSpec {
-  "base schema validation" should {
-    "fail closure period is string" in {
+
+  "Base schema validation" should {
+
+    "fail when closure_period is a string" in {
       val schemaPath = "metadata-schema/baseSchema.schema.json"
       val testDataPath = "/data/testDataClosurePeriod.json"
-      val inputStream = getClass.getResourceAsStream(testDataPath)
-      val jsonString = scala.io.Source.fromInputStream(inputStream).mkString
-      inputStream.close()
-      val data = jsonString.replace("CLOSURE_PERIOD", "3")
+      val modifiedData = loadAndModifyTestData(testDataPath, "CLOSURE_PERIOD", "3")
       val schemaSetup = createTheSchema(schemaPath)
 
-      val errors: util.Set[ValidationMessage] = schemaSetup.validate(data, InputFormat.JSON)
+      val errors: util.Set[ValidationMessage] = schemaSetup.validate(modifiedData, InputFormat.JSON)
       val errorsArray = errors.asScala.toArray
 
-      errorsArray(0).getMessage shouldBe "$.closure_period: string found, [array, null] expected"
+      errorsArray.head.getMessage shouldBe "$.closure_period: string found, [array, null] expected"
     }
-    "fail closure period one value above max" in {
+
+    "fail when closure_period has a value above the maximum limit" in {
       val schemaPath = "metadata-schema/baseSchema.schema.json"
       val testDataPath = "/data/testDataClosurePeriod.json"
-      val inputStream = getClass.getResourceAsStream(testDataPath)
-      val jsonString = scala.io.Source.fromInputStream(inputStream).mkString
-      inputStream.close()
-      val data = jsonString.replace("\"CLOSURE_PERIOD\"", "[1,160]")
+      val modifiedData = loadAndModifyTestData(testDataPath, "\"CLOSURE_PERIOD\"", "[1,160]")
       val schemaSetup = createTheSchema(schemaPath)
 
-      val errors: util.Set[ValidationMessage] = schemaSetup.validate(data, InputFormat.JSON)
+      val errors: util.Set[ValidationMessage] = schemaSetup.validate(modifiedData, InputFormat.JSON)
       val errorsArray = errors.asScala.toArray
 
-      errorsArray(0).getMessage shouldBe "$.closure_period[[1]: must have a maximum value of 150]"
+      errorsArray.head.getMessage shouldBe "$.closure_period[1]: must have a maximum value of 150"
+    }
+  }
+
+  private def loadAndModifyTestData(testDataPath: String, replace: String, replacement: String): String = {
+    Option(getClass.getResourceAsStream(testDataPath)) match {
+      case Some(inputStream) =>
+        Using.resource(inputStream) { stream =>
+          val jsonString = Source.fromInputStream(stream).mkString
+          jsonString.replace(replace, replacement)
+        }
+      case None =>
+        throw new IllegalArgumentException(s"Resource not found: $testDataPath")
     }
   }
 }
