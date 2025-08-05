3f56dc923267b139d7b10cfecc7d9c2c9e42750b
TomJKing <tom.king@nationalarchives.gov.uk>
2025-08-05 15:00:09 +0100

    Additional unit test

--- src/test/scala/uk/gov/nationalarchives/tdr/schemautils/ConfigUtilsSpec.scala
+++ src/test/scala/uk/gov/nationalarchives/tdr/schemautils/ConfigUtilsSpec.scala
@@ -75,7 +75,13 @@ class ConfigUtilsSpec extends AnyWordSpec {
   "ConfigUtils should load configuration and provide a getMetadataProperties method that" should {
     "give the list of properties which has given property type" in {
       val metadataConfiguration = ConfigUtils.loadConfiguration
-      metadataConfiguration.getPropertiesByPropertyType("System") shouldBe List("file_path", "file_name", "date_last_modified", "file_size", "UUID", "rights_copyright", "file_reference", "original_identifier", "parent_reference", "file_type", "client_side_checksum", "server_side_checksum")
+      metadataConfiguration.getPropertiesByPropertyType("System") shouldBe
+        List("file_path", "file_name", "date_last_modified", "file_size", "UUID", "rights_copyright", "file_reference",
+          "original_identifier", "parent_reference", "file_type", "client_side_checksum", "server_side_checksum")
+      metadataConfiguration.getPropertiesByPropertyType("Supplied") shouldBe
+        List("end_date", "description", "former_reference_department", "closure_type", "closure_start_date", "closure_period",
+          "foi_exemption_code", "foi_exemption_asserted", "title_closed", "description_closed", "description_alternate", "title_alternate",
+          "language", "file_name_translation", "restrictions_on_use", "held_by", "legal_status")
       metadataConfiguration.getPropertiesByPropertyType("unknown") shouldBe List()
     }
   }
