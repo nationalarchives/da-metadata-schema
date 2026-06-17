9df9de2bb8d15d2dcec5763daf61251546a6d876
TomJKing <tom.king@nationalarchives.gov.uk>
2026-06-17 09:09:51 +0100

    Set 'original identifier' as supplied property

--- src/test/scala/uk/gov/nationalarchives/tdr/schemautils/ConfigUtilsSpec.scala
+++ src/test/scala/uk/gov/nationalarchives/tdr/schemautils/ConfigUtilsSpec.scala
@@ -95,11 +95,11 @@ class ConfigUtilsSpec extends AnyWordSpec {
       val metadataConfiguration = ConfigUtils.loadConfiguration
       metadataConfiguration.getPropertiesByPropertyType("System") shouldBe
         List("file_path", "file_name", "date_last_modified", "file_size", "UUID", "file_reference",
-          "original_identifier", "parent_reference", "file_type", "client_side_checksum", "server_side_checksum")
+          "parent_reference", "file_type", "client_side_checksum", "server_side_checksum")
       metadataConfiguration.getPropertiesByPropertyType("Supplied") shouldBe
         List("end_date", "description", "former_reference_department", "closure_type", "closure_start_date", "closure_period",
           "foi_exemption_code", "foi_exemption_asserted", "title_closed", "description_closed", "description_alternate", "title_alternate",
-          "language", "file_name_translation", "rights_copyright", "copyright_details", "restrictions_on_use", "held_by", "legal_status",
+          "language", "file_name_translation", "rights_copyright", "copyright_details", "restrictions_on_use", "held_by", "original_identifier", "legal_status",
           "related_material","judgment_type", "judgment_update", "judgment_update_type", "judgment_update_details", "judgment_neutral_citation", "judgment_no_neutral_citation", "judgment_reference", "evidence_provided_by",
           "date_created", "date_range", "file_name_translation_language", "start_date", "creating_body", "file_name_language", "note")
       metadataConfiguration.getPropertiesByPropertyType("unknown") shouldBe List()
@@ -109,7 +109,7 @@ class ConfigUtilsSpec extends AnyWordSpec {
   "ConfigUtils should load configuration and provide a downloadProperties method that" should {
     "give the downloadProperties config for a specified download" in {
       val metadataConfiguration = ConfigUtils.loadConfiguration
-      metadataConfiguration.downloadFileDisplayProperties("MetadataDownloadTemplate").length shouldBe 23
+      metadataConfiguration.downloadFileDisplayProperties("MetadataDownloadTemplate").length shouldBe 24
       metadataConfiguration.downloadFileDisplayProperties("BagitExportTemplate").length shouldBe 38
       metadataConfiguration.downloadFileDisplayProperties("MetadataReviewDetailTemplate").length shouldBe 26
       metadataConfiguration.downloadFileDisplayProperties("UnknownClientTemplate").length shouldBe 0
