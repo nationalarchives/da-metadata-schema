7dd178d25ec7bcc2e4634032594c1651de6e9fd2
VimleshGupta <vimlesh.gupta@nationalarchives.gov.uk>
2026-09-11 14:10:16 +0100

    Fix tests

--- src/test/scala/uk/gov/nationalarchives/tdr/schemautils/ConfigUtilsSpec.scala
+++ src/test/scala/uk/gov/nationalarchives/tdr/schemautils/ConfigUtilsSpec.scala
@@ -133,7 +133,7 @@ class ConfigUtilsSpec extends AnyWordSpec {
       val metadataConfiguration = ConfigUtils.loadConfiguration
       metadataConfiguration.downloadFileDisplayProperties("MetadataDownloadTemplate").length shouldBe 23
       metadataConfiguration.downloadFileDisplayProperties("BagitExportTemplate").length shouldBe 38
-      metadataConfiguration.downloadFileDisplayProperties("MetadataReviewDetailTemplate").length shouldBe 26
+      metadataConfiguration.downloadFileDisplayProperties("MetadataReviewDetailTemplate").length shouldBe 27
       metadataConfiguration.downloadFileDisplayProperties("UnknownClientTemplate").length shouldBe 0
     }
 
